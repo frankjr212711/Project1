5e51a3da95a56f7e864ff8cdcd0998235dc3393d
frankJayRepo <frankjr212711@gmail.com>
2021-03-18 23:58:42 +0000

    checkboxCompleted

--- script.js
+++ script.js
@@ -13,17 +13,44 @@
                   }       
             });    
       })
+      
 })();
 
 
-document.getElementById('close').addEventListener('click', function() {
-      const checkboxContent = this.parentElement;
-      checkboxContent.remove();
+
+// checkbox
+const checkboxes = [...document.querySelectorAll('.item input')];
+let lastChecked;
+
+function handleCheck(e) {
+      let inBetween = false;
+      if(e.shiftKey && this.checked) {
+            checkboxes.forEach(checkbox => {
+                  console.log(checkbox);
+                  if(checkbox === this || checkbox === lastChecked) {
+                        inBetween = !inBetween;
+                  }
+                  if(inBetween) {
+                        checkbox.checked = true; 
+                   }
+            })
+      }
+      lastChecked = this;
+}
 
 
+      checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('click', handleCheck) 
+      });
 
+//      close
+      const close = [...document.querySelectorAll('.close')];
+      
+      
+      close.forEach(el => {
+            el.addEventListener('click', function(e) {
+                  const checkboxContent = this.parentElement;
+                    checkboxContent.remove();
+              });
+      })
 
-
-
-
-});
